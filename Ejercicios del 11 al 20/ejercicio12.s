5307a2be767a768f376c5e724324da44cbd8f48d
Fernanglel <167027961+Fernanglel@users.noreply.github.com>
2024-11-11 14:24:09 -0800

    Update ejercicio12.s

--- Ejercicios del 11 al 20/ejercicio12.s
+++ Ejercicios del 11 al 20/ejercicio12.s
@@ -7,6 +7,7 @@
 //Encontrar el máximo en un arreglo	
 //Recorrido de arreglos	
 //Análisis de datos, búsqueda de valores
+//video - https://asciinema.org/a/8VxML2CQMtIAMhem4HAcgHU1E
 ///////////////////////////////////////////////
 
 //C#
