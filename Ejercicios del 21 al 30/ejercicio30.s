cb52fa4a4a540e7c745a5fc8b544e7c9cf146afd
Fernanglel <167027961+Fernanglel@users.noreply.github.com>
2024-11-11 14:22:46 -0800

    Update ejercicio30.s

--- Ejercicios del 21 al 30/ejercicio30.s
+++ Ejercicios del 21 al 30/ejercicio30.s
@@ -7,10 +7,36 @@
 //Máximo Común Divisor (MCD)	
 //Aritmética, algoritmo de Euclides	
 //Simplificación de fracciones
+//video - 
 ///////////////////////////////////////////////
 
 //C#
 /*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 56;
+        int b = 98;
+
+        // Llamar a la función recursiva para calcular el MCD
+        int mcd = CalcularMCDRecursivo(a, b);
+
+        // Mostrar el resultado
+        Console.WriteLine($"El MCD de {a} y {b} es: {mcd}");
+    }
+
+    // Función recursiva para calcular el MCD usando el algoritmo de Euclides
+    static int CalcularMCDRecursivo(int a, int b)
+    {
+        if (b == 0)
+            return a;
+        else
+            return CalcularMCDRecursivo(b, a % b); // Llamada recursiva
+    }
+}
 
 */
 
