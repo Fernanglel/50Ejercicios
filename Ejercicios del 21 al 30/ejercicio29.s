0adf53640098623c2daba723a4601dd72ef6a0eb
Fernanglel <167027961+Fernanglel@users.noreply.github.com>
2024-11-11 14:21:54 -0800

    Update ejercicio29.s

--- Ejercicios del 21 al 30/ejercicio29.s
+++ Ejercicios del 21 al 30/ejercicio29.s
@@ -7,10 +7,41 @@
 //Contar los bits activados en un número
 //Operaciones bit a bit	
 //Procesamiento de datos, compresión
+//video - 
 ///////////////////////////////////////////////
 
 //C#
 /*
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 29;  // 29 en binario es 11101
+
+        // Contar los bits activados utilizando la conversión a binario
+        int cantidadBitsActivados = ContarBitsActivadosBinario(num);
+
+        // Mostrar el resultado
+        Console.WriteLine($"El número {num} tiene {cantidadBitsActivados} bits activados (en 1).");
+    }
+
+    // Método para contar los bits activados en un número utilizando la representación binaria
+    static int ContarBitsActivadosBinario(int num)
+    {
+        string binario = Convert.ToString(num, 2);  // Convertir el número a binario
+        int cuenta = 0;
+
+        // Contar los '1' en la representación binaria
+        foreach (char bit in binario)
+        {
+            if (bit == '1') cuenta++;
+        }
+
+        return cuenta;
+    }
+}
 
 */
 
