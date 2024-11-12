65384bcae84be491137de6400304bd30c503487b
Fernanglel <167027961+Fernanglel@users.noreply.github.com>
2024-11-12 13:35:55 -0800

    Update ejercicio35.s

--- Ejercicios del 31 al 40/ejercicio35.s
+++ Ejercicios del 31 al 40/ejercicio35.s
@@ -9,8 +9,17 @@
 //Algoritmos de criptografía
 ///////////////////////////////////////////////
 
-//C#
+//Python
 /*
+def rotar_izquierda(arr, n):
+    n = n % len(arr)  # Asegurarse de que 'n' no sea mayor que la longitud del arreglo
+    return arr[n:] + arr[:n]
+
+# Ejemplo de uso
+arr = [1, 2, 3, 4, 5]
+n = 2
+resultado = rotar_izquierda(arr, n)
+print(f"Rotación a la izquierda por {n} posiciones: {resultado}")
 
 */
 
