007270754b1e3f2921c9fbcc479b94a1d7409e69
Fernanglel <167027961+Fernanglel@users.noreply.github.com>
2024-11-12 13:36:32 -0800

    Update ejercicio36.s

--- Ejercicios del 31 al 40/ejercicio36.s
+++ Ejercicios del 31 al 40/ejercicio36.s
@@ -11,6 +11,45 @@
 
 //C#
 /*
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeros = { 5, 12, 3, 9, 21, 19 };
+
+        if (numeros.Length < 2)
+        {
+            Console.WriteLine("El arreglo no tiene suficientes elementos.");
+            return;
+        }
+
+        int max1 = int.MinValue, max2 = int.MinValue;
+
+        foreach (int num in numeros)
+        {
+            if (num > max1)
+            {
+                max2 = max1;  // Actualizar el segundo mayor
+                max1 = num;   // Actualizar el mayor
+            }
+            else if (num > max2 && num != max1)
+            {
+                max2 = num;  // Actualizar el segundo mayor si es menor que max1
+            }
+        }
+
+        if (max2 == int.MinValue)
+        {
+            Console.WriteLine("No hay un segundo elemento más grande.");
+        }
+        else
+        {
+            Console.WriteLine($"El segundo elemento más grande es: {max2}");
+        }
+    }
+}
 
 */
 
